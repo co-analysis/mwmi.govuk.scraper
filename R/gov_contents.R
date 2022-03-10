1e6f0e64e50c04b737c6198dd3607108bff146f7
Josh <joshua.wallace@cabinetoffice.gov.uk>
2022-03-10 17:12:05 +0000

    gov_contents description

--- R/gov_contents.R
+++ R/gov_contents.R
@@ -1,6 +1,11 @@
 #' Check gov.uk links for potential files
 #'
-#' Check gov.uk search results for potential files to download
+#' Takes a vector of results for pages and scrapes links to embedded data
+#' returns a list as long as the input with:
+#' url: original url requested
+#' meta_data: data.table of meta data including org and date
+#' data_titles: a vector of text titles for data files
+#' data_links: a vector of links to data files
 #'
 #' @param links links on gov.uk to check for files to download
 #' @import httr
