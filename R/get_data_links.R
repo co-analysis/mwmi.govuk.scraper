398fc5678d811db42e786c96b9285dd75e25d3c6
Josh <joshua.wallace@cabinetoffice.gov.uk>
2023-09-12 16:21:31 +0100

    Update to allow downloading links other than spreadsheets

--- R/get_data_links.R
+++ R/get_data_links.R
@@ -3,9 +3,10 @@
 #' Internal function
 #'
 #' @param url_to_scrape url on gov.uk
+#' @param spreadsheets_only (default=TRUE) filters links to only return those which have a spreadsheet file type
 #' @import rvest
 
-get_data_links <- function(url_to_scrape) {
+get_data_links <- function(url_to_scrape, spreadsheets_only=TRUE) {
   # Extracting content
   html_text <- rvest::read_html(url_to_scrape) # no error catching...
 
@@ -27,8 +28,7 @@ get_data_links <- function(url_to_scrape) {
   #                                     ., value = TRUE, ignore.case = TRUE)
   data_links <- html_text %>%
     rvest::html_nodes(".gem-c-attachment .gem-c-attachment__details a") %>%
-    rvest::html_attr("href") %>%
-    grep("((ods)|(csv)|(xls)|(xlsx))$", ., value = TRUE, ignore.case = TRUE)
+    rvest::html_attr("href")
 
   # Find text titles for files
   # data_titles <- html_text %>%
@@ -37,6 +37,13 @@ get_data_links <- function(url_to_scrape) {
   data_titles <- html_text %>%
     rvest::html_nodes(".gem-c-attachment .gem-c-attachment__details h2") %>%
     rvest::html_text(trim=TRUE)
+
+  if (spreadsheets_only==TRUE) {
+    spreads = grepl("((ods)|(csv)|(xls)|(xlsx))$", data_links, ignore.case = TRUE)
+    data_links <- data_links[spreads]
+    data_titles <- data_titles[spreads]
+  }
+
 
   print(paste0(length(data_links)," results from ",url_to_scrape))
 
--- R/gov_contents.R
+++ R/gov_contents.R
@@ -8,13 +8,14 @@
 #' data_links: a vector of links to data files
 #'
 #' @param links links on gov.uk to check for files to download
+#' @param spreadsheets_only (default=TRUE) filters links to only return those which have a spreadsheet file type
 #' @import httr
 #' @export
 
-gov_contents <- function(links) {
+gov_contents <- function(links,spreadsheets_only=TRUE) {
   # url format
   urls <- map(links, ~ modify_url("https://www.gov.uk/api/content", path = .))
 
   # Get the data links
-  map(urls,get_data_links_limited)
+  map(urls,get_data_links_limited,spreadsheets_only=spreadsheets_only)
 }
